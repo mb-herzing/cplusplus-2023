3c650e9c539c2adb388452e9fd65d35759bcab20
mb-herzing <mbeder@herzing.edu>
2023-11-21 09:38:57 -0600

    Beginning Unit 4 game
    Creating outline for Unit 4 game, including classes and inheritance with objects soon to follow.

---
+++ object_game.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+//  Player superclass
+
+//  Player Subclass - Fighter
+
+//  Player Subclass - Ranger
+
+//  Player Subclass - Wizard
+
+//  Enemy superclass
+
+//  Enemy Subclass = Goblin
+
+//  Enemy Subclass - Skeleton
+
+//  Enemy Subclass - Slime
+
+//  Add a boss? Lich or something?
+
+//  Yes/No option class
+
+int main() {
+    bool gameStarted = false;
+
+    
+}
