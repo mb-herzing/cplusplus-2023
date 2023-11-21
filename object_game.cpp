66683d237a697510604d54bbae63ab41aa7858ea
mb-herzing <mbeder@herzing.edu>
2023-11-21 10:20:43 -0600

    Testing player input
    Creating general method for getting yes/no input from player. Also providing a brief story for the game.

--- object_game.cpp
+++ object_game.cpp
@@ -20,10 +20,42 @@ using namespace std;
 
 //  Add a boss? Lich or something?
 
-//  Yes/No option class
+//  Method for providing player imput upon recieving a yes/no prompt
+bool getYesNo() {
+    bool answeredYes = false;
+    string input;
+    cin >> input;
+    if (input == "y" || input == "Y") {
+        answeredYes = true;
+    }
+    else if (input == "n" || input == "N") {
+        answeredYes = false;
+    }
+    return answeredYes;
+};
 
 int main() {
-    bool gameStarted = false;
+    //  Provide some opening story to the player
+    cout << "The terrible dungeon in the far edges of Sylvaestos has only been held back ";
+    cout << "by countless adventurers delving inside with hopes of peace and promises of riches.";
+    cout << endl << "Will you be the one to claim the foretold treasure and end this strife, ";
+    cout << "or will you fall in battle like the rest?" << endl;
 
-    
+    //  Prompt player to continue
+    cout << "Do you wish to continue? (y/n)" << endl;
+
+    bool gameStarted = false;
+    gameStarted = getYesNo();
+    if (gameStarted) {
+        //  Testing
+        cout << "Game Started!";
+    }
+    else {
+        //  Bid farewell to the player
+        cout << "Perhaps another time. The dungeon will await you...";
+        //  Return 1 (Player Exit)
+        return 1;
+    }
+    //  Return 0 (Exit Success)
+    return 0;
 }
